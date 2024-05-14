30648ae4c26ee33c9039c9069270eed0bfe932d1
ThomasTrepanier <thomtrepanier@gmail.com>
2024-05-14 18:52:39 -0400

    Trying to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/BattleMaisonSearch',
     plugins: [react()],
     resolve: {
         alias: {
